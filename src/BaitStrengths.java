dea373bf93a672adae0b23afc47fad08a54f9bf4
SUSAltd <the_pgf@yahoo.com>
2014-01-17 12:50:02 -0800

    Some experimental baits

---
+++ src/BaitStrengths.java
@@ -0,0 +1,33 @@
+
+public class BaitStrengths {
+	public static final Bait[] BAITS = {
+		new Bait("the Master Bait", 3.0),
+		new Bait("the Luck of the Sea", 2.5),
+		new Bait("a lure worthy of weekly worship", 2.3),
+		new Bait("an ol' chum", 2.0),
+		new Bait("an undebateably great bait", 1.7),
+		new Bait("a wriggly worm", 1.3),
+		new Bait("phish phood", 1.2),
+		new Bait("some standard-issue bait", 1.1),
+		new Bait("a detriment to all purposes fishing-related", 0.5)
+	};
+//	
+//	private final ProbabilityPair[] BAITS_WEIGHTED = {
+//		new ProbabilityPair(new Bait("the Master Bait", 3.0), 0.01),
+//		new ProbabilityPair(new Bait("a Lure Worthy of Weekly Worship", 2.3), 0.10),
+//		new ProbabilityPair(new Bait("Ol' Buddy, Ol' Chum", 2.0), 0.10)
+//	};
+//	
+//	
+//	
+//	
+//	private class ProbabilityPair {
+//		private Bait key;
+//		private double weight;
+//		
+//		public ProbabilityPair(Bait key, double weight) {
+//			this.key = key;
+//			this.weight = weight;
+//		}
+//	}
+}
