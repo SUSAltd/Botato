dd0e2de8868f8f96ec697ceb2f9e763d2ec7f7b8
SUSAltd <the_pgf@yahoo.com>
2014-02-13 23:50:26 -0800

    Removed unused code

--- src/BaitStrengths.java
+++ src/BaitStrengths.java
@@ -11,23 +11,4 @@ public class BaitStrengths {
 		new Bait("some standard-issue bait", 1.1),
 		new Bait("a detriment to all purposes fishing-related", 0.5)
 	};
-//	
-//	private final ProbabilityPair[] BAITS_WEIGHTED = {
-//		new ProbabilityPair(new Bait("the Master Bait", 3.0), 0.01),
-//		new ProbabilityPair(new Bait("a Lure Worthy of Weekly Worship", 2.3), 0.10),
-//		new ProbabilityPair(new Bait("Ol' Buddy, Ol' Chum", 2.0), 0.10)
-//	};
-//	
-//	
-//	
-//	
-//	private class ProbabilityPair {
-//		private Bait key;
-//		private double weight;
-//		
-//		public ProbabilityPair(Bait key, double weight) {
-//			this.key = key;
-//			this.weight = weight;
-//		}
-//	}
 }
